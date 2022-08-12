2bef5d457ccfd660b8efcc1442b3ca8c25676128
l-makour <lahcene.makour@gmail.com>
2022-08-12 18:38:18 +0100

    init project

---
+++ src/main/java/com/checkconsulting/markatplace/MarkatplaceApplication.java
@@ -0,0 +1,13 @@
+package com.checkconsulting.markatplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkatplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarkatplaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/checkconsulting/markatplace/MarkatplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkconsulting.markatplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkatplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
