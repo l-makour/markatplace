e555de4d746edfab237a095319de316cc88eb816
mahdi chekini <me.chekini@gmail.com>
2022-08-18 11:46:43 +0200

    Add env Controller

---
+++ src/main/java/com/checkconsulting/markatplace/controller/ApplicationController.java
@@ -0,0 +1,22 @@
+package com.checkconsulting.markatplace.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationController {
+
+
+    @Value("${env}")
+    String env;
+
+
+    @GetMapping("api/v1/env")
+    public String getEnv() {
+        return env;
+    }
+
+
+}
